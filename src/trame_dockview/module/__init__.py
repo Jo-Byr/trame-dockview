418dc2731c27b9e1b5e3db4d0e63c1af36918f09
Jo-Byr <jonathanbouyer24@gmail.com>
2026-09-17 16:02:17 +0200

    chore: update JS dependencies

--- src/trame_dockview/module/__init__.py
+++ src/trame_dockview/module/__init__.py
@@ -6,5 +6,5 @@ serve_path = str(Path(__file__).with_name("serve").resolve())
 server_path = f"__trame_dockview_{__version__}"
 serve = {server_path: serve_path}
 scripts = [f"{server_path}/trame_dockview.umd.js"]
-styles = [f"{server_path}/style.css"]
+styles = [f"{server_path}/trame_dockview.css"]
 vue_use = ["trame_dockview"]
